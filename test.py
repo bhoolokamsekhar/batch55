dac0a781897681d89b34ac72ced36b1285a14143
bhoolokamsekhar <bholokams@gmail.com>
2026-05-14 19:06:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-print("hi hello sekhar")
-a=10
-b=20
-a+=b
-print(a)
+pipeline {
+    agent any
+
+    stages {
+        stage('Test') {
+            steps {
+                script {
+                    echo "hello world"
+
+                    def a = 10
+                    def b = 20
+                    def c = a + b
+
+                    echo "Result is: ${c}"
+                }
+            }
+        }
+    }
+}
